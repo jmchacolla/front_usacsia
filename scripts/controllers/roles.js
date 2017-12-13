a5985a216737b29889d69b67a46b9c5b804d2b49
Wendy Fuertes <wendyfu19@gmail.com>
2017-12-13 13:59:07 -0400

    roles.js

--- scripts/controllers/roles.js
+++ scripts/controllers/roles.js
@@ -16,12 +16,6 @@ angular.module('adminApp')
 
 //DOMINIO_SERVICIOS: "http://localhost:8000"//---------------------jhon
 
-
-
-
-
-
-
 //wendy
 DOMINIO_SERVICIOS: "http://localhost:8080/api_usacsia/public"
   // DOMINIO_SERVICIOS: "http://localhost/usacsia_php_5.6/usacsia_git/api_usacsia/public"
