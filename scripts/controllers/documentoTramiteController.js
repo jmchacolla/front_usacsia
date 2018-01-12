f7d3d89cfb54bddc729bc63782e8ac995abb1f58
vero <vronk_tcviral@hotmail.com>
2018-01-12 18:16:05 -0300

    subir documentos

--- scripts/controllers/documentoTramiteController.js
+++ scripts/controllers/documentoTramiteController.js
@@ -1,8 +1,8 @@
 'use strict';
 angular.module("adminApp")
 
-.controller('CrearDocumentoTramiteCtrl', ['$scope', 'DocumentoTramite', '$route','$routeParams', 'toastr', '$location',
-function ($scope, DocumentoTramite, $route,$routeParams,toastr,$location)
+.controller('CrearDocumentoTramiteCtrl', ['$scope','DocumentoNoRegistrado','DocumentoRegistrado', 'DocumentoTramite', '$route','$routeParams', 'toastr', '$location',
+function ($scope, DocumentoNoRegistrado,DocumentoRegistrado,DocumentoTramite, $route,$routeParams,toastr,$location)
 {
   $scope.ajustes = {
     menu:{
@@ -14,80 +14,59 @@ function ($scope, DocumentoTramite, $route,$routeParams,toastr,$location)
       titulo:'Documentos requeridos para el registro del establecimiento'
     }
   }
-
-  console.log('LLEGO AL CONTROLADOR del crear docmuento---------');
   
   $scope.sortType = 'per_id'; // set the default sort type
   $scope.sortReverse  = true;  // set the default sort order
   $scope.Personas = [];
-  var et_id= $routeParams.et_id;
-
-  $scope.obs=false;
-  $scope.cambio = function(){
-    if($scope.obs){
-      $scope.obs=false;
-    }else{
-      $scope.obs=true;
-    }
-    console.log('raidoooooooos',$scope.obs);
-  }
-    $scope.obs2=false;
-  $scope.cambio2 = function(){
-    if($scope.obs2){
-      $scope.obs2=false;
-    }else{
-      $scope.obs2=true;
-    }
-    console.log('raidoooooooos',$scope.obs2);
-  }
-    $scope.obs3=false;
-  $scope.cambio3 = function(){
-    if($scope.obs3){
-      $scope.obs3=false;
-    }else{
-      $scope.obs3=true;
-    }
-    console.log('raidoooooooos',$scope.obs3);
-  }
+  var et_id=2; //se debe obtener el ultimo tramite del establecimiento
   
-  $scope.documentoTramite1={
-    doc_id:1,
-    et_id:et_id,
-    dt_url: "./img-nit",
-    dt_observacion:"",
-  }
+  $scope.documento_select={
+    doc_id:null,
+    doc_nombre:""
+  };
 
-    $scope.documentoTramite2={
-    doc_id:1,
-    et_id:et_id,
-    dt_url: "./img-licfun",
-    dt_observacion:"",
-  }
+  DocumentoNoRegistrado.get({et_id:et_id},function(data){
+    $scope.documentos_no_registraos=data.documento;
+    if( $scope.documentos_no_registraos.length==0){
+      $scope.documentos_no_registraos=null;
+    }
+  });
 
-  $scope.documentoTramite3={
+
+  DocumentoRegistrado.get({et_id:et_id},function(data){
+    $scope.documentos_registraos=data.documento;
+  });
+
+  $scope.documentoTramite={
     doc_id:1,
     et_id:et_id,
-    dt_url: "./img-ci",
-    dt_observacion:"",
+    dt_url: "./img-documentos"
   }
-  
-    $scope.submit = function(a){
-      if(a==1){
-        $scope.documentoTramite=$scope.documentoTramite1;
+  var dtid=null;
+  $scope.get_id=function(dt_id,doc_nombre){
+    dtid=dt_id;
+    $scope.doc_nombre=doc_nombre;
+  };
+
+  $scope.eliminar_doctra = function(){
+     DocumentoTramite.delete({dt_id:dtid}).$promise.then(function(data)
+    {
+      if(data.status)
+      {
+        $route.reload();
       }
-      if(a==2){
-        $scope.documentoTramite=$scope.documentoTramite2;
-      }
-      if(a==3){
-        $scope.documentoTramite=$scope.documentoTramite3;
-      }
-      
-    console.log($scope.documentoTramite, "documento que se va a guardar",$scope.documentoTramite,a);
+    })
+  };
+
+    $scope.submit = function(){
+      $scope.documentoTramite.doc_id=$scope.documento_select.doc_id;
+    console.log($scope.documentoTramite, "documento que se va a guardar",$scope.documentoTramite);
 
     DocumentoTramite.save($scope.documentoTramite).$promise.then(function(data){
       if(data.msg){
         $scope.ajustes.pagina.success = "El ciudadano registrada exitosamente";
         toastr.success('Documento Guardado correctamente');
+        $route.reload();
       }
     });
   }
