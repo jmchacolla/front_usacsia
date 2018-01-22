49f0aa484acc14a52f882995fa688de9d5b953c8
vero <vronk_tcviral@hotmail.com>
2018-01-22 20:20:03 -0300

    mensaje

--- scripts/controllers/roles.js
+++ scripts/controllers/roles.js
@@ -27,7 +27,7 @@ angular.module('adminApp')
 
 
 //vero
-DOMINIO_SERVICIOS: "http://localhost/usacsia_php_5.6/solo_api_dorys_git/api_usacsia_v1_0/public"
+DOMINIO_SERVICIOS: "http://192.168.6.223/usacsia_php_5.6/solo_api_dorys_git/api_usacsia_v1_0/public"
 
 
 // DOMINIO_SERVICIOS: "http://192.168.6.223/usacsia_php_5.6/solo_api_dorys_git/api_usacsia_v1_0/public"
